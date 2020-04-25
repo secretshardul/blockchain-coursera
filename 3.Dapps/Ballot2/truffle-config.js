5dc601f91e95c3b6e62f74290cdf7c573afc8b94
HP <hp@HP.local>
2020-04-25 16:53:22 +0530

    Dapps

---
+++ 3.Dapps/Ballot2/truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.17"  // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+  }
+};
